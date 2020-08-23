02b75d0536ee3fae3f85450c365cf7e6d78b11dd
rajan <royal_raj@outlook.com>
2020-08-23 11:06:11 +0545

    makes about dynamic

--- src/components/About.js
+++ src/components/About.js
@@ -1,68 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const About = () => {
+
+    const [about, setAbout] = useState({
+        title: 'about',
+        subTitle: 'Lorem ipsum dolor sit amet consectetur.',
+        subHeading: 'Our Humble Beginnings',
+        timeLine: [
+            {
+                year: '2009-2011',
+                subHeading: 'Our Humble Beginnings',
+                timeLineBody: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!',
+                timeLineInverted: false
+            },
+            {
+                year: 'March 2011',
+                subHeading: 'Our Humble Beginnings',
+                timeLineBody: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!',
+                timeLineInverted: true
+            },
+            {
+                year: 'january 2011',
+                subHeading: 'Our Humble Beginnings',
+                timeLineBody: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!',
+                timeLineInverted: false
+            },
+        ]
+    });
+
     return (
         <section className="page-section" id="about">
-        <div className="container">
-            <div className="text-center">
-                <h2 className="section-heading text-uppercase">About</h2>
-                <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+            <div className="container">
+                <div className="text-center">
+                    <h2 className="section-heading text-uppercase">{about.title}</h2>
+                    <h3 className="section-subheading text-muted">{about.subTitle}</h3>
+                </div>
+                <ul className="timeline">
+                    {about.timeLine.map(x => {
+                        return (
+                            <li key={x.year} className={x.timeLineInverted ? "timeline-inverted" : ""} >
+                                <div className="timeline-image"><img className="rounded-circle img-fluid" src="assets/img/about/1.jpg" alt="" /></div>
+                                <div className="timeline-panel">
+                                    <div className="timeline-heading">
+                                        <h4>{x.year}</h4>
+                                        <h4 className="subheading">{x.subHeading}</h4>
+                                    </div>
+                                    <div className="timeline-body"><p className="text-muted">{x.timeLineBody}</p></div>
+                                </div>
+                            </li>
+                        )
+                    })}
+                    <li class="timeline-inverted">
+                        <div class="timeline-image">
+                            <h4>
+                                Be Part
+                                <br />
+                                Of Our
+                                <br />
+                                Story!
+                            </h4>
+                        </div>
+                    </li>
+                </ul>
             </div>
-            <ul className="timeline">
-                <li>
-                    <div className="timeline-image"><img className="rounded-circle img-fluid" src="assets/img/about/1.jpg" alt="" /></div>
-                    <div className="timeline-panel">
-                        <div className="timeline-heading">
-                            <h4>2009-2011</h4>
-                            <h4 className="subheading">Our Humble Beginnings</h4>
-                        </div>
-                        <div className="timeline-body"><p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
-                    </div>
-                </li>
-                <li className="timeline-inverted">
-                    <div className="timeline-image"><img className="rounded-circle img-fluid" src="assets/img/about/2.jpg" alt="" /></div>
-                    <div className="timeline-panel">
-                        <div className="timeline-heading">
-                            <h4>March 2011</h4>
-                            <h4 className="subheading">An Agency is Born</h4>
-                        </div>
-                        <div className="timeline-body"><p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
-                    </div>
-                </li>
-                <li>
-                    <div className="timeline-image"><img className="rounded-circle img-fluid" src="assets/img/about/3.jpg" alt="" /></div>
-                    <div className="timeline-panel">
-                        <div className="timeline-heading">
-                            <h4>December 2012</h4>
-                            <h4 className="subheading">Transition to Full Service</h4>
-                        </div>
-                        <div className="timeline-body"><p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
-                    </div>
-                </li>
-                <li className="timeline-inverted">
-                    <div className="timeline-image"><img className="rounded-circle img-fluid" src="assets/img/about/4.jpg" alt="" /></div>
-                    <div className="timeline-panel">
-                        <div className="timeline-heading">
-                            <h4>July 2014</h4>
-                            <h4 className="subheading">Phase Two Expansion</h4>
-                        </div>
-                        <div className="timeline-body"><p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
-                    </div>
-                </li>
-                <li className="timeline-inverted">
-                    <div className="timeline-image">
-                        <h4>
-                            Be Part
-                        <br />
-                        Of Our
-                        <br />
-                        Story!
-                    </h4>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    </section>
+        </section>
     );
 }
 
--- src/components/Team.js
+++ src/components/Team.js
@@ -1,50 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Team = () => {
+    const [team, setTeam] = useState({
+        title: 'Our Amazing Team',
+        subTitle: 'Lorem ipsum dolor sit amet consectetur.',
+        description:'Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.',
+        members: [
+            {
+                id: 1,
+                name: 'Kay Garland',
+                position: 'Lead Designer',
+                image: "assets/img/team/1.jpg",
+                twitter: "www.twitter.com",
+                facebook: "",
+                linkedin: ""
+            },
+            {
+                id: 2,
+                name: 'Ray Garland',
+                position: 'Lead Designer',
+                image: "assets/img/team/2.jpg",
+                twitter: "www.twitter.com",
+                facebook: "",
+                linkedin: ""
+            },
+            {
+                id: 3,
+                name: 'Shey Garland',
+                position: 'Lead Designer',
+                image: "assets/img/team/3.jpg",
+                twitter: "www.twitter.com",
+                facebook: "",
+                linkedin: ""
+            }
+        ]
+    });
     return (
         <section className="page-section bg-light" id="team">
-        <div className="container">
-            <div className="text-center">
-                <h2 className="section-heading text-uppercase">Our Amazing Team</h2>
-                <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-            </div>
-            <div className="row">
-                <div className="col-lg-4">
-                    <div className="team-member">
-                        <img className="mx-auto rounded-circle" src="assets/img/team/1.jpg" alt="" />
-                        <h4>Kay Garland</h4>
-                        <p className="text-muted">Lead Designer</p>
-                        <a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-twitter"></i></a>
-                        <a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-facebook-f"></i></a>
-                        <a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-linkedin-in"></i></a>
-                    </div>
+            <div className="container">
+                <div className="text-center">
+                    <h2 className="section-heading text-uppercase">{team.title}</h2>
+                    <h3 className="section-subheading text-muted">{team.subTitle}</h3>
                 </div>
-                <div className="col-lg-4">
-                    <div className="team-member">
-                        <img className="mx-auto rounded-circle" src="assets/img/team/2.jpg" alt="" />
-                        <h4>Larry Parker</h4>
-                        <p className="text-muted">Lead Marketer</p>
-                        <a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-twitter"></i></a>
-                        <a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-facebook-f"></i></a>
-                        <a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-linkedin-in"></i></a>
-                    </div>
+                <div className="row">
+                    {team.members.map(member => {
+                        return (
+                            <div className="col-lg-4">
+                                <div className="team-member">
+                                    <img className="mx-auto rounded-circle" src={member.image} alt="" />
+                                    <h4>{member.name}</h4>
+                                    <p className="text-muted">{member.position}</p>
+                                    <a className="btn btn-dark btn-social mx-2" href={member.twitter}><i className="fab fa-twitter"></i></a>
+                                    <a className="btn btn-dark btn-social mx-2" href={member.facebook}><i className="fab fa-facebook-f"></i></a>
+                                    <a className="btn btn-dark btn-social mx-2" href={member.linkedin}><i className="fab fa-linkedin-in"></i></a>
+                                </div>
+                            </div>
+                        )
+                    })}
                 </div>
-                <div className="col-lg-4">
-                    <div className="team-member">
-                        <img className="mx-auto rounded-circle" src="assets/img/team/3.jpg" alt="" />
-                        <h4>Diana Petersen</h4>
-                        <p className="text-muted">Lead Developer</p>
-                        <a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-twitter"></i></a>
-                        <a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-facebook-f"></i></a>
-                        <a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-linkedin-in"></i></a>
-                    </div>
+                <div className="row">
+                <div className="col-lg-8 mx-auto text-center"><p className="large text-muted">{team.description}</p></div>
                 </div>
             </div>
-            <div className="row">
-                <div className="col-lg-8 mx-auto text-center"><p className="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p></div>
-            </div>
-        </div>
-    </section>
+        </section>
     );
 }
 
